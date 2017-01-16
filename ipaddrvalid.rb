dba525ea13694f820a08c6559af4b0e40105a606
imAkshay26 <akshay@joshsoftware.com>
2017-01-16 11:37:23 +0530

    modified

--- ipaddrvalid.rb
+++ ipaddrvalid.rb
@@ -1,7 +1,7 @@
 puts "enter ip address:"
 #this is incomplete
 ip = gets.chomp!
-if ip =~ /\A[1-2][0-9][0-9].[1-2][0-9][0-9].[1-2][0-9][0-9]).([1-2][0-9][0-9]\z/
+if ip =~ /\A([0-1]\d{,2}|2[0-5]{,2}).([0-1]\d{,2}|2[0-5]{,2}).([0-1]\d{,2}|2[0-5]{,2}).([0-1]\d{,2}|2[0-5]{0,2})\z/
 	puts "valid ip"
 else
 	puts "invalid ip"
